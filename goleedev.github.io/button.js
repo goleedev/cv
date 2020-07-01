26dadb4403c5a3ed294107464e2726a69d6be1bc
GO Lee <52603436+goleedev@users.noreply.github.com>
2020-07-01 21:42:46 +0900

    Fix responsiveness

---
+++ goleedev.github.io/button.js
@@ -0,0 +1,15 @@
+var btn = $('#footer-pointing');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').stop().animate({ scrollTop: '300' });
+});
+
